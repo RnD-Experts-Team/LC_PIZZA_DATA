1605566635492b0437dd41d88669459f0413efe0
Aws12505 <awsaljaramani12505@gmail.com>
2026-05-28 12:55:47 -0400

    ..

--- app/Services/EventConsume/Handlers/EmployeeCreatedHandler.php
+++ app/Services/EventConsume/Handlers/EmployeeCreatedHandler.php
@@ -112,7 +112,7 @@ class EmployeeCreatedHandler implements EventHandlerInterface
         $latest = $this->latestEntry($statuses);
         $status = strtolower((string) data_get($latest, 'status', ''));
 
-        return in_array($status, ['hired', 'rehired', 'OJE'], true);
+        return in_array($status, ['hired', 'rehired', 'oje'], true);
     }
 
     private function latestEntry(array $items): ?array
