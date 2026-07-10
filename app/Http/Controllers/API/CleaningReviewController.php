33f91cba29739cef65ae752c4c6d7cc9261405f8
Aws12505 <awsaljaramani12505@gmail.com>
2026-07-10 10:17:31 -0400

    ..

--- app/Http/Controllers/API/CleaningReviewController.php
+++ app/Http/Controllers/API/CleaningReviewController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\API\Concerns\HandlesCsvFileUpload;
 use App\Services\CleaningReviewCsvProcessor;
+use App\Services\CustomerServiceCsvProcessor;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -19,6 +20,16 @@ class CleaningReviewController extends Controller
             'cleaning_review_uploads',
             'cleaning_review_',
             fn (string $filePath) => (new CleaningReviewCsvProcessor())->process($filePath)
+        );
+    }
+
+    public function uploadCustomerServiceCsv(Request $request): JsonResponse
+    {
+        return $this->processCsvUpload(
+            $request,
+            'customer_service_uploads',
+            'customer_service_',
+            fn (string $filePath) => (new CustomerServiceCsvProcessor())->process($filePath)
         );
     }
 }
